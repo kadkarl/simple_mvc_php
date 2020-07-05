b4a3f0589359a0fd4de5aae0ab9363a786760e34
Bendahmane <abkder@gmail.com>
2020-07-05 22:41:21 +0200

    Ajout Tests

---
+++ src/app/Tests/Models/NobelModelTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Models;
+
+use App\Models\NobelModel;
+use PHPUnit\Framework\TestCase;
+
+class NobelModelTest extends TestCase
+{
+    public function testOk()
+    {
+        $this->assertEquals(20, 10 + 10);
+    }
+    
+
+}
